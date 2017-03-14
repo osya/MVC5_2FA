4a5a28e9a26945aca67c82c0e8e2f0fdb26e2a21
Valeriy Osipov <info@vosipov.com>
2017-03-14 13:12:33 +0300

    Add all changed files

---
+++ MvcPWy/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace MvcPWy.Controllers
+{
+   public class HomeController : Controller
+   {
+    public ActionResult Index()
+    {
+        ViewBag.Link = TempData["ViewBagLink"];  
+        return View();
+    }
+
+      public ActionResult About()
+      {
+         ViewBag.Message = "Your application description page.";
+
+         return View();
+      }
+
+    [Authorize]
+    public ActionResult Contact()
+    {
+       ViewBag.Message = "Your contact page.";
+
+       return View();
+    }
+   }
+}
